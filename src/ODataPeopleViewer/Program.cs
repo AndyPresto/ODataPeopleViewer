6d91f0ec80bc572f8c7a591fa7a99dac93eceb6b
Andrew Preston <andy@assetpad.app>
2021-08-21 19:00:18 +0700

    Added code to solution

---
+++ src/ODataPeopleViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ODataPeopleViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
